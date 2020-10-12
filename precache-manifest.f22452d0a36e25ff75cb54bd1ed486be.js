9a7440bde4041f9b70b7e95e7ffc69125722590f
jagadeesh <nakkajagadeesh1234@gmail.com>
2020-10-12 22:06:08 +0530

    Updates

---
+++ precache-manifest.f22452d0a36e25ff75cb54bd1ed486be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4245783e61b813f0ec88de04e7082039",
+    "url": "/pizzaBox/index.html"
+  },
+  {
+    "revision": "0f7f881ca0981346ba2d",
+    "url": "/pizzaBox/static/css/main.2d3e2d6e.chunk.css"
+  },
+  {
+    "revision": "9cdda1fabe6efe6d49a4",
+    "url": "/pizzaBox/static/js/2.a5b8b484.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizzaBox/static/js/2.a5b8b484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f7f881ca0981346ba2d",
+    "url": "/pizzaBox/static/js/main.75226edc.chunk.js"
+  },
+  {
+    "revision": "79022b5b202aed51a5d1",
+    "url": "/pizzaBox/static/js/runtime-main.624170ef.js"
+  },
+  {
+    "revision": "65666f57ccec39f522c0692c38a26640",
+    "url": "/pizzaBox/static/media/minus.65666f57.svg"
+  },
+  {
+    "revision": "a5edc4bde6f249ff8a05d71aa0185fd0",
+    "url": "/pizzaBox/static/media/plus.a5edc4bd.svg"
+  }
+]);
